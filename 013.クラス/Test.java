08d3ac6bed74ff6b2e654d1b3bb93fa84659fe4b
堀田　華愛 <kanonduut@gmail.com>
2018-05-21 20:02:33 +0900

    クラス課題1が終了しました。ご確認お願いいたします。

---
+++ 013.クラス/Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author タカ
+ */
+public class Test {
+    public static void main(String[] height){
+        Human hito=new Human();
+      hito.setHuman("堀田",158);
+      hito.print();
+      
+    
+    }
+    
+    
+    
+}
