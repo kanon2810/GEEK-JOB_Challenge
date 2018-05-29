089fe807ec52d3c50def4930347ebeb29911fb94
堀田　華愛 <kanonduut@gmail.com>
2018-05-29 14:25:51 +0900

    標準クラス課題6が終了いたしました

---
+++ 015.標準クラス/MOJI2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templatesr
+ * and open the template in the editor.
+ */
+package kadai;
+
+/**
+ *
+ * @author タカ
+ */
+public class MOJI2 {
+    public static void main(String[] agrs){
+    String mail="kanoncuut@Gmail.com";
+    
+    
+ 
+   System.out.print(mail.substring(9,19).length());
+   
+   
+    
+    
+    
+    
+    
+    }
+}
