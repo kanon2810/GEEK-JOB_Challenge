3c73b28374681320e88697bb0741d90160b392d6
堀田　華愛 <kanonduut@gmail.com>
2018-05-29 14:08:02 +0900

    標準クラス課題5が終了いたしました

---
+++ 015.標準クラス/MOJI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kadai;
+
+
+
+/**
+ *
+ * @author タカ
+ */
+public class MOJI {
+    public static void main(String[] args){
+ 
+String name="ほりたかのん";
+
+System.out.print("文字数は全部で"+name.length()+"です");
+System.out.println("住所のバイト数は" + name.getBytes().length + "です。");
+
+
+    }
+}
