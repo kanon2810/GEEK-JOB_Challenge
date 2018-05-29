8d1894a489ebf30ab68624422ad1113eaa65d5bf
堀田　華愛 <kanonduut@gmail.com>
2018-05-29 14:56:23 +0900

    標準クラス課題7が終了いたしました

---
+++ 015.標準クラス/MOJI3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kadai;
+
+/**
+ *
+ * @author タカ
+ */
+public class MOJI3 {
+    public static void main(String[] agrs){
+        String wakewakaran="きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        
+        
+       wakewakaran= wakewakaran.replace("I","い");
+       wakewakaran= wakewakaran.replace("U","う");
+        
+       System.out.print(wakewakaran);
+      
+
+    }
+}
