ba7c9836e5f4fa7c666b5851088bbda8a683edca
堀田　華愛 <kanonduut@gmail.com>
2018-06-05 10:12:29 +0900

    セッション課題1が終了しました

---
+++ 016.データ操作/session.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author タカ
+ */
+@WebServlet(urlPatterns = {"/session"})
+public class session extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet session</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+       //セッションの取得
+      //HttpSession　変数名=requesut.getSession();
+      
+    //セッションからデータの読みだし
+    //HttpSession変数.getAttribute(名前);
+    //セッションへのデータの書き込み
+    //HttpSession変数.setAttribute(名前,情報);
+    
+    Date z=new Date();
+    String toki2=String.valueOf(z);
+    //最後に開いた時間を表示する
+    //セッションの取得
+    HttpSession time=request.getSession();
+    //セッションの読み出し
+    time.getAttribute("BASE");
+    
+   //セッションから情報を取得
+    String DATE=(String)time.getAttribute("BASE");
+    
+    //セッションへのデータの書き込み
+    time.setAttribute("BASE", toki2);
+    
+    out.print(DATE);
+
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
